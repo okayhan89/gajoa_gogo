1c0648dbd6ca78bdcf5e32ecdf6453d372a21bc7
한승엽(HAN SEOUNG YEOB)/통신/미디어Digital추진그룹/SK <09707@skcc.com>
2020-02-28 15:34:45 +0900

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Content from "./Component/Content"
+import Content2 from "./Component/Content2"
+import Content3 from "./Component/Content3"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {/* <Content></Content>  */}
+       {/* <Content2></Content2>  */}
+       <Content3></Content3> 
       </header>
     </div>
   );
