fc1e5c9f2be7adb73376ed6b403ab82506338610
한승엽 <hanseung-yeob@hanseung-yeob-ui-MacBook-Air.local>
2020-03-07 10:33:53 +0900

    abc

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Content from "./Component/Content"
 import Content2 from "./Component/Content2"
 import Content3 from "./Component/Content3"
-
+import Header from "./Header/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
        {/* <Content></Content>  */}
        {/* <Content2></Content2>  */}
